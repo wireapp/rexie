64884d2b7f1374adacec89f50da03b63ef0703a3
Mathieu Amiot <amiot.mathieu@gmail.com>
2024-03-07 19:27:14 +0100

    fix: Remove wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -109,12 +109,6 @@ mod rexie_builder;
 mod transaction;
 mod utils;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 pub use self::{
     direction::Direction,
     error::{Error, Result},
